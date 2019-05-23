9eafe91206d86c04e68a59a8db9235b6b71b6651
Austin Davis <austindavis@Austins-2017-MacBook-Pro.local>
2019-05-22 23:05:12 -0700

    game working

---
+++ testproject/Memory.swift
@@ -0,0 +1,45 @@
+//
+//  Memory.swift
+//  Memory
+//
+//  Created by Austin Davis on 5/22/19.
+//  Copyright © 2019 Austin Davis. All rights reserved.
+//
+
+import Foundation
+
+class Memory {
+    
+    var cards = Array<Card>()
+    
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
+    func chooseCard(at index: Int) {
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                // check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                // either no or 2 cards are face up
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+    }
+    
+    init(numberOfPairsOfCards: Int) {
+        for _ in 0..<numberOfPairsOfCards {
+            let card = Card()
+            cards += [card, card]
+        }
+        // TODO: Shuffle the cards
+    }
+}
--- testproject/ViewController.swift
+++ testproject/ViewController.swift
@@ -10,33 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
   
-  var flipCount = 0 {
-    didSet {
-      flipCountLabel.text = "Flips: \(flipCount)"
+    lazy var game = Memory(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
+    
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
     }
-  }
 
-  @IBOutlet weak var flipCountLabel: UILabel!
-  
-  @IBOutlet var cardButtons: [UIButton]!
-  
-  var emojiChoices = ["🐣", "🦉", "🐣", "🦉"]
-  
-  @IBAction func touchCard(_ sender: UIButton) {
-    flipCount += 1
-    if let cardNumber = cardButtons.firstIndex(of: sender) {
-      flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+    @IBOutlet weak var flipCountLabel: UILabel!
+
+    @IBOutlet var cardButtons: [UIButton]!
+
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+            game.chooseCard(at: cardNumber)
+            updateViewFromModel()
+        }
     }
-  }
-  
-  func flipCard(withEmoji emoji: String, on button: UIButton) {
-    if button.currentTitle == emoji {
-      button.setTitle("", for: UIControl.State.normal)
-      button.backgroundColor = #colorLiteral(red: 0.9858418107, green: 0.60793221, blue: 0, alpha: 1)
-    } else {
-      button.setTitle(emoji, for: UIControl.State.normal)
-      button.backgroundColor = #colorLiteral(red: 0.6666666865, green: 0.6666666865, blue: 0.6666666865, alpha: 1)
+    
+    func updateViewFromModel() {
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isFaceUp {
+                button.setTitle(emoji(for: card), for: UIControl.State.normal)
+                button.backgroundColor = #colorLiteral(red: 0.6666666865, green: 0.6666666865, blue: 0.6666666865, alpha: 1)
+            } else {
+                button.setTitle("", for: UIControl.State.normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 0.9858418107, green: 0.60793221, blue: 0, alpha: 0) : #colorLiteral(red: 0.9858418107, green: 0.60793221, blue: 0, alpha: 1)
+            }
+        }
     }
-  }
+    
+    var emojiChoices = ["🐣", "🦉"]
+    
+    var emoji = Dictionary<Int,String>()
+    
+    func emoji(for card: Card) -> String {
+        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+        }
+        return emoji[card.identifier] ?? "?"
+    }
 }
 
